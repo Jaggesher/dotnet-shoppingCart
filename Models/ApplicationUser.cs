22606766c4b6abcbaccbe2ac292fb8f63efa5a90
jaggesher <jaggesher14@gmail.com>
2018-04-05 12:21:33 +0600

    database and migrations basic added

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace dotnet_shoppingCart.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_shoppingCart.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,9 @@ namespace dotnet_shoppingCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
