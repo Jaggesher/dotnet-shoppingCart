16d4ddba9dec75a898da7b488d8d7cef6f7e0ecd
jaggesher <jaggesher14@gmail.com>
2018-04-05 13:05:41 +0600

    JWT service Updated

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using dotnet_shoppingCart.Data;
+using dotnet_shoppingCart.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace dotnet_shoppingCart
 {
@@ -28,6 +33,56 @@ namespace dotnet_shoppingCart
             services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            //services.AddSingleton<IJwtFactoryService,JwtFactoryService>();
+           
+
+            var jwtAppsettingsOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtAppsettingsOptions["SecreatKey"]));
+
+            services.Configure<JwtIssuerOptions>(Options =>
+                {
+                    Options.Issuer = jwtAppsettingsOptions[nameof(JwtIssuerOptions.Issuer)];
+                    Options.Audience = jwtAppsettingsOptions[nameof(JwtIssuerOptions.Audience)];
+                    Options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+                });
+        
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppsettingsOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppsettingsOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(Options =>
+                {
+                    Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(configureOptions =>
+                {
+                    configureOptions.ClaimsIssuer = jwtAppsettingsOptions[nameof(JwtIssuerOptions.Issuer)];
+                    configureOptions.TokenValidationParameters = tokenValidationParameters;
+                    configureOptions.SaveToken = true;
+                });
+
+            services.AddAuthorization(Options =>
+                {
+                    Options.AddPolicy("ApiUser", policy => policy.RequireClaim("rol","ApiAccess"));
+                });
+                
             services.AddMvc();
         }
 
@@ -39,6 +94,7 @@ namespace dotnet_shoppingCart
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
